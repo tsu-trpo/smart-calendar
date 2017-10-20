743eaf516d4bff88746d918fa849790d96ba54f6
فلاديمير ألاموف <alamovvladimir@gmail.com>
2017-10-20 22:14:45 +0700

    [+]Попытка вывести текст

---
+++ Assets/scripts/text.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class text : MonoBehaviour {
+
+    public Text data_text;
+    public DateTime Now = DateTime.Now;
+    // Use this for initialization
+    void Start () {
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Now.Text = Now;
+    }
+}
---
+++ Assets/scripts/today.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class today : MonoBehaviour
+{
+
+    // Use this for initialization
+    DateTime Now = DateTime.Now;
+    void Start()
+    {
+        //print(Now.DayOfWeek + " " +Now.ToString());
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(100, 100, 100, 100), Now.ToString());
+    }
+
+}

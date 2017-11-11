227e87c9c14d8532af2455cedfeae7d6acd3032b
kranonetka <fakemail4steam@ya.ru>
2017-11-11 17:30:29 +0700

    [+] Test script for initizalize prefab

---
+++ Assets/Day/Scripts/Cloner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour {
+
+    public GameObject target;
+    public Transform container;
+
+    void OnMouseUp()
+    {
+            GameObject.Instantiate(target, new Vector3(Random.Range(-3, 3), Random.Range(-5, 5), 0), Quaternion.identity, container);
+    }
+}

df2bfc6029ab86484b2a2bec5f2adad95c8983ae
Usufjon <yusufjonov98@gmail.com>
2017-11-21 10:44:17 -0900

    Painted cells for days

---
+++ Assets/Title/Scripts/CellCreate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellCreate : MonoBehaviour {
+
+
+    private static float x =-2.5f, y=3.5f;
+
+        public GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+        for(int i=0; i<5; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                GameObject.Instantiate(target, new Vector3(x, y, 0), Quaternion.identity);
+                x += 0.8f;
+            }
+            y -= 0.8f;
+            x = -2.5f;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

2f6ef60bd042d7db8652acceab4ef446a23ce7c9
kranonetka <fakemail4steam@ya.ru>
2017-12-23 15:20:28 +0700

    [+] Issue manager depends on date

--- Assets/Day/Scripts/IssueManager.cs
+++ Assets/Day/Scripts/IssueManager.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class IssueManager : MonoBehaviour
 {
+    private static Dictionary<System.DateTime, List<GameObject>> issuesMap = new Dictionary<System.DateTime, List<GameObject>>();
 
     public GameObject dialogWindow, prototype, closeButton;
     public Transform issuesContainer;
@@ -9,8 +11,26 @@ public class IssueManager : MonoBehaviour
     DialogWindowManager dialogWindowManager;
     CloseButtonDriver closeButtonDriver;
 
+    private System.DateTime selectedDate = System.DateTime.Now;
+
+    public void SetDate(System.DateTime date)
+    {
+        selectedDate = date;
+        Transform[] childrens = issuesContainer.GetComponentsInChildren<Transform>();
+        foreach(Transform children in childrens)
+        {
+            Destroy(children.gameObject);
+        }
+        foreach(GameObject issueCell in issuesMap[selectedDate])
+        {
+            Instantiate(issueCell, issuesContainer);
+
+        }
+    }
+
     private void Awake()
     {
+
         dialogWindowManager = dialogWindow.GetComponent<DialogWindowManager>();
         dialogWindow.SetActive(false);
         closeButtonDriver = closeButton.GetComponent<CloseButtonDriver>();
@@ -19,9 +39,8 @@ public class IssueManager : MonoBehaviour
     private GameObject spawnIssue(string issueText, string issueTime)
     {
         GameObject issueClone = prototype;
-        UnityEngine.UI.Text[] texts = issueClone.GetComponentsInChildren<UnityEngine.UI.Text>();
-        texts[0].text = issueText;
-        texts[1].text = issueTime;
+        issueClone.GetComponent<Issue>().IssueText = issueText;
+        issueClone.GetComponent<Issue>().IssueText = issueTime;
         return issueClone;
     }
 
@@ -32,7 +51,8 @@ public class IssueManager : MonoBehaviour
             if (dialogWindowManager.IssueText.Length > 3) //Create new issue if lenght > 3
             {
                 string selectedTime = dialogWindowManager.SelectedHours + ":" + dialogWindowManager.SelectedMinutes;
-                Instantiate(spawnIssue(dialogWindowManager.IssueText, selectedTime), issuesContainer);
+                GameObject newIssue = Instantiate(spawnIssue(dialogWindowManager.IssueText, selectedTime), issuesContainer) as GameObject;
+                issuesMap[selectedDate].Add(newIssue);
                 dialogWindowManager.Deactivate();
                 closeButtonDriver.Deactivate();
             }
---
+++ Assets/Issue.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Issue : MonoBehaviour
+{
+    [SerializeField]
+    private Text issueText, issueTime;
+
+    public string IssueText
+    {
+        get
+        {
+            return issueText.text;
+        }
+        set
+        {
+            issueText.text = value;
+        }
+    }
+
+    public string IssueTime
+    {
+        get
+        {
+            return issueTime.text;
+        }
+        set
+        {
+            issueTime.text = value;
+        }
+    }
+}

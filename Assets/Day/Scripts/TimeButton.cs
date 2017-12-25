846f35e3dd04c2a57017856f859de018150aee9e
kranonetka <fakemail4steam@ya.ru>
2017-12-25 22:22:22 +0700

    [+] Time Button class

---
+++ Assets/Day/Scripts/TimeButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeButton : MonoBehaviour {
+
+	public Text buttonTextUI;
+
+    string ButtonText
+    {
+        get
+        {
+            return buttonTextUI.GetComponentInChildren<Text>().text;
+        }
+        set
+        {
+            buttonTextUI.GetComponentInChildren<Text>().text = value;
+        }
+    }
+
+    public void setName(string newName)
+    {
+        name = newName;
+    }
+}

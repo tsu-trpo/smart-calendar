5c604fbcbbfd9d2cc118f7535b74f3090bc91ea0
Usufjon <yusufjonov98@gmail.com>
2017-12-27 11:07:46 +0700

    [+] change backround of choosen cell, [~] fixed some bags

--- Assets/Title/Scripts/Cell.cs
+++ Assets/Title/Scripts/Cell.cs
@@ -8,6 +8,9 @@ public class Cell : MonoBehaviour
 {
     private Text text;
     private DateTime date;
+    private MonthView month;
+    private int number;
+    private bool isExist = true;  //activity, true is basic state;
 
     public void SetDate(DateTime _date)
     {
@@ -25,18 +28,41 @@ public class Cell : MonoBehaviour
         text.color = Color.black;
     }
 
-    public void SetColorToday ()
+    public void SetColorToday()
     {
         text.color = Color.red;
     }
 
     private void OnMouseUp()
     {
+        month = this.transform.GetComponentInParent<MonthView>();
         Debug.Log("Clicked! on " + date.ToString("yyyy MMMM dd"));
+        month.ChangePushed(number);
     }
 
     private void Awake()
     {
         text = this.gameObject.transform.GetChild(0).GetComponent<Text>();
+    }
+
+    public void ChangeState()
+    {
+        isExist = !isExist;
+        gameObject.SetActive(isExist);
+    }
+
+    public void SetBackgroundOn()
+    {
+        this.transform.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    public void SetBackgroundOff()
+    {
+        this.transform.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void SetNumber(int _number)
+    {
+        number = _number;
     }
 }
